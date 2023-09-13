500aa73fea720daabc11f4897a611ec84a90e26c
masoodazam <140827933+masoodazam@users.noreply.github.com>
2023-09-13 18:50:50 +0530

    variable.tf

--- variable.tf
+++ variable.tf
@@ -4,11 +4,9 @@ resource "local_file" "new_pet" {
 }
 
 variable "filename" {
-  type    = string
   default = "new_pets.txt"
 }
 
 variable "file_content" {
-  type    = string
   default = "Our new pet is a cute kitten!"
 }
